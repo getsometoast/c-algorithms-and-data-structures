ab7e792259cdfa1181fd8da254539278039791b4
getsometoast <james.lewis@7digital.com>
2014-02-06 23:15:33 +0000

    update dictionary

---
+++ data-structures/dictionary.c
@@ -0,0 +1,14 @@
+/* a dictionary offers the following operations:
+ * - Search
+ *   Insert
+ *   Delete
+ *   Successor
+ *   Predecessor
+ *   Minimum
+ *   Maximum
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int Search(int key);
