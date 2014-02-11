45b04bb70959f22db96fa077827785fb2391b8b6
getsometoast <james.lewis@7digital.com>
2014-02-11 21:00:17 +0000

    add some implementations of a dictionary with balanced binary tree

--- data-structures/dictionary.c
+++ data-structures/dictionary.c
@@ -11,27 +11,88 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+typedef struct node {
+  
+  int value;
+  int key;
+
+  node *leftChild;
+  node *rightChild;
+
+} node;
+
 typedef struct dictionary {
 
-  int data[10];
+  node *root;
+
 } dictionary;
 
-int Search(dictionary *dict, int value);
-void Insert(int value);
-void Delete(int key);
-int Successor(int Key);
-int Predecessor(int key);
-int Minimum();
-int Maximum();
+node* Search(node *root, int key);
+node* Insert(node *root, node value);
+void Delete(dictionary *dict, node *value);
+int Successor(dictionary *dict, int Key);
+int Predecessor(dictionary *dict, int key);
+int Minimum(dictionary *dict);
+int Maximum(dictionary *dict);
 
-int Search(dictionary *dict, int value) {
+int main() {
   
+  dictionary dict = malloc(sizeof(dictionary));
+  node newItem = malloc(sizeof(node));
+  newItem->value = 123;
+  newItem->key = 1;
+
+  dict = Insert(dict, newItem);
+  dict = Insert(dict, newItem);
+  dict = Insert(dict, newItem);
+
+  return 0;
 }
 
-void Insert(dictionary *dict, int value) {
+/* returns a pointer to the node or NULL if node not found.
+ * could run in linear time if the tree not balanced properly.
+ * */
+node* Search(node *root, int key) {
+  
+  if(key == root->key)
+    return &root;
 
+  if(root->leftChild == NULL && root->rightChild == NULL)
+    return NULL;
+  else if(key < root->key)
+    Search(root->leftChild);
+  else
+    Search(root->rightChild);
 }
 
-void Delete(dictionary *dict, int key) {
+/* returns pointer to the current root of the tree after the value has been added. 
+ * must maintain the balanced tree (this currently doesn't)
+ * */
+node* Insert(node *root, item value) {
+  
+  if(value->key == root->key)
+    return &root; /* no duplicates */
 
+  if(value->key < root->key && root->leftChild == NULL)
+  {
+    root->leftChild = value;
+    return theRoot;
+  }
+
+  if(value->key > root->key && root->rightChild == NULL)
+  {
+    root->rightChild = value;
+    return theRoot;
+  }
+  else if(value->key < root->key)
+    Insert(root->leftChild, value);
+  else if(value->key > root->key)
+    Insert(root->rightChild, value);
 }
+
+/* constant time */
+void Delete(node *head, item *value) {
+  /* need to remove the value but also make sure the tree is still balanced. */
+}
+
+
